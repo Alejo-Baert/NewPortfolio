add6a0ec7f8f4d1f401c561e3b88852aed55df40
Alejo Baert <alejobaert4444@outlook.com>
2024-01-19 12:27:17 -0300

    assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,39 +1,39 @@
 // SVG
-// import logoWhite from './svg/logoWhite.svg'
-import logoBlack from './svg/logoBlack.svg'
-import modeBlack from './svg/ModeBlack.svg'
-import modeWhite from './svg/ModeWhite.svg'
-import hamburguer from './svg/Hamburguer.svg'
-import react from './svg/react.svg'
-import nextjs from './svg/nextjs.svg'
-import tailwind from './svg/tailwind.svg'
-import figma from './svg/figma.svg'
-import javascript from './svg/javascript.svg'
-import arrowUrl from './svg/arrowUrl.svg'
-import download from './svg/download.svg'
-import emailIcon from './svg/email.svg'
-import arrowTop from './svg/arrowTop.svg'
-import emailIconWhite from './svg/emailWhite.svg'
-import SoundOn from './svg/SoundOn.svg'
-import mute from './svg/mute.svg'
-import close from './svg/close.svg'
+import logoWhite from '/svg/logoWhite.svg'
+import logoBlack from '/svg/logoBlack.svg'
+import modeBlack from '/svg/ModeBlack.svg'
+import modeWhite from '/svg/ModeWhite.svg'
+import hamburguer from '/svg/Hamburguer.svg'
+import react from '/svg/react.svg'
+import nextjs from '/svg/nextjs.svg'
+import tailwind from '/svg/tailwind.svg'
+import figma from '/svg/figma.svg'
+import javascript from '/svg/javascript.svg'
+import arrowUrl from '/svg/arrowUrl.svg'
+import download from '/svg/download.svg'
+import emailIcon from '/svg/email.svg'
+import arrowTop from '/svg/arrowTop.svg'
+import emailIconWhite from '/svg/emailWhite.svg'
+import SoundOn from '/svg/SoundOn.svg'
+import mute from '/svg/mute.svg'
+import close from '/svg/close.svg'
 
-import behance from './svg/behance.svg'
-import linkedin from './svg/linkedin.svg'
-import instagram from './svg/instagram.svg'
-import github from './svg/github.svg'
+import behance from '/svg/behance.svg'
+import linkedin from '/svg/linkedin.svg'
+import instagram from '/svg/instagram.svg'
+import github from '/svg/github.svg'
 
-import grain from './img/grain.png'
+import grain from '/img/grain.png'
 
 
 // Knowledge
-import dotAzul from './img/knowledge/dotAzul.png'
-import dotRojo from './img/knowledge/dotRojo.png'
-import heroBg from './img/knowledge/heroBg.jpg'
-import mainBg from './img/knowledge/mainBg.jpg'
+import dotAzul from '/img/knowledge/dotAzul.png'
+import dotRojo from '/img/knowledge/dotRojo.png'
+import heroBg from '/img/knowledge/heroBg.jpg'
+import mainBg from '/img/knowledge/mainBg.jpg'
 
 export {
-    // logoWhite,
+    logoWhite,
     logoBlack,
     modeBlack,
     modeWhite,
