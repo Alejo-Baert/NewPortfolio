47015aab72eae07c3d14361985ef3d10ecde303c
Alejo Baert <alejobaert4444@outlook.com>
2024-01-22 08:52:36 -0300

    resumidor

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,19 +6,21 @@ import pp1 from '../assets/img/projects/pp1.png'
 import preciosabio from '../assets/img/projects/preciosabio.png'
 import sochic from '../assets/img/projects/sochic.png'
 import futbolapp1 from '../assets/img/projects/futbolapp1.png'
+import resumidor from '../assets/img/projects/resumidor.png'
 import futbolapp2 from '../assets/img/projects/futbolapp2.png'
 
 import { linkedin, instagram, behance, github, dotAzul, dotRojo } from "../assets";
 
 export const imageProjects = [
-    { id: 1, img: preciosabio, titulo: 'Precio Sabio', año: '2023', numero: '#8', link: 'https://preciosabio.netlify.app/' },
-    { id: 2, img: futbolapp1, titulo: 'Futbol Argentino App', año: '2023', numero: '#7', link: 'https://futbol-argentino-app.vercel.app/' },
+    { img: preciosabio, titulo: 'Precio Sabio', año: '2023', numero: '#8', link: 'https://preciosabio.netlify.app/' },
+    { img: resumidor, titulo: 'Resumidor IA', año: '2023', numero: '#7', link: 'https://ab-resumidor-ia.netlify.app/' },
+    { img: futbolapp1, titulo: 'Futbol Argentino App', año: '2023', numero: '#6', link: 'https://futbol-argentino-app.vercel.app/' },
     // { id: 3, img: milei, titulo: 'Milei: Juego de Memoria', año: '2023', numero: '#6', link: 'https://mileimemoria.netlify.app/' },
-    { id: 4, img: firstportfolio, titulo: 'First Portfolio', año: '2022', numero: '#5', link: 'https://alejobaert.netlify.app/' },
-    { id: 5, img: lyriks, titulo: 'Lyriks', año: '2022', numero: '#4', link: 'https://lyriks-alejobaert.netlify.app/' },
-    { id: 6, img: argentina, titulo: 'Argentina', año: '2022', numero: '#3', link: 'https://argentina-website.netlify.app/' },
-    { id: 7, img: sochic, titulo: 'So Chic', año: '2021', numero: '#2', link: 'https://sochic.netlify.app/' },
-    { id: 8, img: pp1, titulo: 'PP1', año: '2020', numero: '#1', link: 'https://alejobaert.neocities.org/' },
+    { img: firstportfolio, titulo: 'First Portfolio', año: '2022', numero: '#5', link: 'https://firstalejobaert.netlify.app/' },
+    { img: lyriks, titulo: 'Lyriks', año: '2022', numero: '#4', link: 'https://lyriks-alejobaert.netlify.app/' },
+    { img: argentina, titulo: 'Argentina', año: '2022', numero: '#3', link: 'https://argentina-website.netlify.app/' },
+    { img: sochic, titulo: 'So Chic', año: '2021', numero: '#2', link: 'https://sochic.netlify.app/' },
+    { img: pp1, titulo: 'PP1', año: '2020', numero: '#1', link: 'https://alejobaert.neocities.org/' },
 ]
 
 export const options = [
